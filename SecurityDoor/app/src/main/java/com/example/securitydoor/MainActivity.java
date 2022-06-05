4c28ec4c90511b1136c3ae6558ab75f0e8c64795
RafaelSaro <94816274+RafaelSaro@users.noreply.github.com>
2022-06-05 17:56:58 -0500

    app

---
+++ SecurityDoor/app/src/main/java/com/example/securitydoor/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.securitydoor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    private WebSettings myWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.web1);
+        myWebSettings = myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebSettings.setDomStorageEnabled(true);
+        myWebView.loadUrl("http://www.rafa23.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+
+
+    }
+}
